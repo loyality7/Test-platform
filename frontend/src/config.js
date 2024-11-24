b8e1c38489ce588bf0f47454fc3fcdeb2678ecd6
C Sarath <sarathyadav112@gmail.com>
2024-11-25 04:46:51 +0530

    working fine

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  API_URL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api'
+  API_URL: process.env.REACT_APP_API_URL || 'https://evaluate-api.hysterchat.com/api'
 };
 
 console.log('Current API Configuration:', config);
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://evaluate-api.hysterchat.com/api';
 
 // Create axios instance with default config
 const axiosInstance = axios.create({
